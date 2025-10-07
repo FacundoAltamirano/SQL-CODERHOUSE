08167aefce680eff066be4b66b2ba17d4f0c7eca
Facundo Altamirano <facugaltamirano@gmail.com>
2025-10-07 15:35:25 -0300

    Pruebas agregadas

---
+++ Desktop/Docs/Prueba-Funcionamiento.sql
@@ -0,0 +1,10 @@
+CTRL + F Para buscar una palabra 
+F3 Para saltar a la siguiente coincidencia
+Shift + Alt +  ↓ Para duplicar linea
+CRTL + L Para seleccionar una linea entera
+Alt + ↑ / Alt + ↓ Mover hacia arriba o abajo
+CTRL + SHIFT + E Para abrir el Explorer
+CTRL + B Para cerrar 
+CTRL + W Para cerrar un archivo 
+CTRL + TAB Para moverse entre archivos 
+CTRL + SHIFT + TAB Para volverse al archivo
